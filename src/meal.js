521290fd89151dbb7eef9579b2c8c1fdbb780119
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2021-12-04 11:26:01 -0600

    Add Meal-test.js

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,62 @@
-function nameMenuItem() {
+function nameMenuItem(menuItemName) {
+return "Delicious " + menuItemName
+}
+
+function createMenuItem(name, price, type) {
+
+  var menuItem = {
+    name: name,
+    price: price,
+    type: type,
+  }
+  return menuItem
+}
+
+function addIngredients(ingredient, ingredients) {
+  for (var i = 0; i < ingredients.length; i++) {
+    if (ingredient === ingredients[i]) {
+      return
+    }
+  }
+  return ingredients.push(ingredient);
+}
+
+//should add a $ in front of price
+//
+
+
+function formatPrice(initialPrice){
+
+return `$${initialPrice}`
 
 }
 
+function decreasePrice(price) {
+  return price * .9
+}
 
+// class Recipe {
+//   constructor() {
+//     this.title = title,
+//     this.price = price,
+//     this.menuItem = createMenuItem
+
+
+function createRecipe(title,ingredients,type) {
+  var recipe = {
+
+    title: title,
+    ingredients: ingredients,
+    type: type,
+
+  }
+return recipe 
+}
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
